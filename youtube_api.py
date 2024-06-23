a6597c9daef30e0fe7b32bb80677a002201b68ba
KishaMulenga <168156052+KishaMulenga@users.noreply.github.com>
2024-06-23 18:38:20 -0500

    Update youtube_api.py

--- youtube_api.py
+++ youtube_api.py
@@ -1,5 +1,5 @@
 from pyyoutube import Api 
-api = Api(api_key="AIzaSyCgNPRtXCtGPMcPCNtR1frL6g5rzeJRMd8")
+api = Api(api_key="YOUR_API_KEY")
 
 channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
 print(channel_by_id.items[0].to_dict())
